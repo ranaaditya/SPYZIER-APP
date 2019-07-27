beb4200d753440f365122bf65ca73805e25c2550
Aditya Rana <42575044+ranaaditya@users.noreply.github.com>
2019-07-27 13:55:51 +0530

    updated

--- app/src/main/java/com/rana_aditya/parent/MainActivity.java
+++ app/src/main/java/com/rana_aditya/parent/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      //  setContentView(R.layout.activity_main);
         NotificationChannel channel = new NotificationChannel("mychannel", "mychannel", NotificationManager.IMPORTANCE_DEFAULT);
            NotificationManager manager = getSystemService(NotificationManager.class);
            manager.createNotificationChannel(channel);
